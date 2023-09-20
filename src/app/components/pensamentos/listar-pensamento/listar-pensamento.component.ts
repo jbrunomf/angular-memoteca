332d74f61d3cda7c9a0647437de2beb94ff16b0c
João Bruno <jbrunomf@outlook.com>
2023-09-19 20:42:51 -0400

    removendo trecho desnecessário

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent implements OnInit{
+export class ListarPensamentoComponent {
 
   listaPensamentos = [
     // {
@@ -24,8 +24,4 @@ export class ListarPensamentoComponent implements OnInit{
     //   modelo: "modelo2"
     // }
   ];
-
-  constructor(){}
-
-  ngOnInit(): void{}
 }
